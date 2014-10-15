12ebd1f03de207bcac26404ded201cec603272f8
Mauro Servienti <mauro@topics.it>
2014-10-15 15:29:35 +0200

    temp

--- v5/NSB01SelfHost/Program.cs
+++ v5/NSB01SelfHost/Program.cs
@@ -14,6 +14,35 @@ namespace NSB01SelfHost
         {
             var cfg = new BusConfiguration();
 
+            //cfg.AutoSubscribe();
+            //cfg.DefineCriticalErrorAction();
+            //cfg.DisableDurableMessages();
+            //cfg.DisableFeature();
+            //cfg.DiscardFailedMessagesInsteadOfSendingToErrorQueue();
+            //cfg.DoNotCreateQueues();
+            //cfg.EnableCriticalTimePerformanceCounter();
+            //cfg.EnableDurableMessages();
+            //cfg.EnableFeature();
+            //cfg.EnableInstallers();
+            //cfg.EnableOutbox();
+            //cfg.EndpointName();
+            //cfg.EndpointVersion();
+            //cfg.License();
+            //cfg.LicensePath();
+            //cfg.LoadMessageHandlers();
+            //cfg.OverrideLocalAddress();
+            //cfg.OverridePublicReturnAddress();
+            //cfg.Pipeline;
+            //cfg.PurgeOnStartup();
+            //cfg.RegisterComponents();
+            //cfg.ScaleOut();
+            //cfg.ScanAssembliesInDirectory();
+            //cfg.SecondLevelRetries();
+            //cfg.Transactions();
+            //cfg.UsePersistence();
+            //cfg.UseSerialization();
+            //cfg.UseTransport();
+
             cfg.UsePersistence<InMemoryPersistence>();
             cfg.Conventions()
                 .DefiningCommandsAs( t => t.Namespace != null && t.Namespace.EndsWith( ".Commands" ) )
